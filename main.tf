5f428e751fa89af25b1f192a4e9ec11219a78644
Anand Bahuguni <anandnb1@yahoo.com>
2024-07-16 13:42:54 +0530

    removed code for passing env variable to terraform.. too complicated

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-variable "environment_var" {
-  description = "The environment (dev, prod)"
-  type = string
-  default = var.TF_ENV
-}
-# # Access the environment variable passed from the workflow
-# variable environment_var = var.TF_ENV
 
 resource "aws_s3_bucket" "my_bucket" {
   # bucket = "cicd-terraform-git-dev"
-  bucket = format("%s-my-bucket", environment_var)
   acl  = "private"
 }
